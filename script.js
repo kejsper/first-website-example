1463612a2ee9440238de1702b1a8ba6d40f43358
kejsper <kejsper@gmail.com>
2017-03-10 18:07:33 +0100

    corrected tabs in the script

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-$(document).ready(function() {
-  $(window).on('scroll', function() {
+$(document).ready(function () {
+  $(window).on('scroll', function () {
     var pozycjaScrolla = $(window).scrollTop();
-
     strzalkaGora(pozycjaScrolla);
     przewijaneMenu(pozycjaScrolla);
   });
-// animuje przewijanie po nacisnieciu strzalki
-  $('#scrolltop').on('click', function(event) {
+  // animuje przewijanie po nacisnieciu strzalki
+  $('#scrolltop').on('click', function (event) {
     event.preventDefault();
     $('html, body').animate({scrollTop: 0}, 700);
   });
-// animuje linki na stronie
-  $('a[href^="#"]').on('click', function(event) {
-		var target = $($(this).attr('href'));
-		if( target.length ) {
-			event.preventDefault();
-			$('html, body').animate({scrollTop: target.offset().top}, 700);
+  // animuje linki na stronie
+  $('a[href^="#"]').on('click', function (event) {
+    var target = $($(this).attr('href'));
+    if ( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({scrollTop: target.offset().top}, 700);
     }
-	});
-// pokazuje strzalke do przewijania strony
+  });
+  // pokazuje strzalke do przewijania strony
   function strzalkaGora(pozycjaScrolla) {
     if (pozycjaScrolla > 100) {
       $('#scrolltop').fadeIn(350);
@@ -27,18 +26,15 @@ $(document).ready(function() {
       $('#scrolltop').fadeOut(350);
     }
   }
-// zweza menu, zmienia rozmiar fontow
+  // zweza menu, zmienia rozmiar fontow
   function przewijaneMenu(pozycjaScrolla) {
     if (pozycjaScrolla > 40) {
-      $('.navbar-duzy').toggleClass('navbar-duzy navbar-maly');
       $('.duze-logo').toggleClass('duze-logo male-logo');
       $('.duze-linki').toggleClass('duze-linki male-linki');
-
     }
     else {
       $('.male-logo').toggleClass('male-logo duze-logo');
       $('.male-linki').toggleClass('male-linki duze-linki');
-      $('.navbar-maly').toggleClass('navbar-maly navbar-duzy');
     }
   }
 });
